9d14773318913ba0291c6e7f54ad7872d261aa05
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2024-01-01 18:11:46 +0100

    fix: last details

--- streem-news/src/app/app.module.ts
+++ streem-news/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { LiteralArrayComponent } from './components/literal-array/literal-array.component';
 import { NewsGraphComponent } from './components/news-graph/news-graph.component';
 import { SearchingAreaComponent } from './components/searching-area/searching-area.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -11,7 +10,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    LiteralArrayComponent,
     NewsGraphComponent,
     SearchingAreaComponent
   ],
