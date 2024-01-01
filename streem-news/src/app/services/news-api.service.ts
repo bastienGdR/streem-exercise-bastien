2b6cece995598b7e310deaa28e5ccd4ec20d969d
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2024-01-01 18:25:53 +0100

    fix: old function in service

--- streem-news/src/app/services/news-api.service.ts
+++ streem-news/src/app/services/news-api.service.ts
@@ -7,7 +7,6 @@ import { IQueryParam } from "../app.component";
     providedIn:'root'
   })
 export class NewsApiService {
-    private url = 'http://localhost:3000/news/search_news?query=scott+morrison&before=1567173600&after=1567045771753';
     private _baseUrl = 'http://localhost:3000/news/search_news';
 
     constructor(private http: HttpClient) {}
@@ -19,10 +18,6 @@ export class NewsApiService {
       
         return `${baseUrl}?${queryString}`;
       }
-
-    public getResultsOld(): Observable<any> {
-        return this.http.get(this.url);
-    }
 
     public getResults(params: IQueryParam): Observable<any> {
         const url = this._buildUrl(this._baseUrl, params);
